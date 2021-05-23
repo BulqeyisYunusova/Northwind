ca96e7a8454811718a8f0a3d155e4d4862c2f47b
BILQEYIS yunuslu <BILQEYIS yunuslu@DESKTOP-2FLSHOI>
2021-05-23 18:26:18 +0400

    commit

--- northwind/src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
+++ northwind/src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import kodlamaio.northwind.business.abstracts.ProductService;
+import kodlamaio.northwind.core.utilities.results.DataResult;
 import kodlamaio.northwind.entities.concretes.Product;
 @RestController
 @RequestMapping("/api/products")
@@ -20,7 +21,7 @@ public class ProductsController {
 		this.productService = productService;
 	}
 	@GetMapping("/getall")
-public List<Product> getAll(){
+public DataResult <List<Product>> getAll(){
 		return this.productService.getAll();
 	
 }
--- northwind/src/main/java/kodlamaio/northwind/business/concretes/ProductManager.java
+++ northwind/src/main/java/kodlamaio/northwind/business/concretes/ProductManager.java
@@ -6,22 +6,36 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kodlamaio.northwind.business.abstracts.ProductService;
+import kodlamaio.northwind.core.utilities.results.DataResult;
+import kodlamaio.northwind.core.utilities.results.Result;
+import kodlamaio.northwind.core.utilities.results.SuccessDataResult;
+import kodlamaio.northwind.core.utilities.results.SuccessResult;
 import kodlamaio.northwind.dataAccess.abstracts.ProductDao;
 import kodlamaio.northwind.entities.concretes.Product;
+
 @Service
 public class ProductManager implements ProductService{
-	private ProductDao productDao;
-	@Autowired
 
+	private ProductDao productDao;
+	
+	@Autowired
 	public ProductManager(ProductDao productDao) {
 		super();
 		this.productDao = productDao;
 	}
 
 	@Override
-	public List<Product> getAll() {
-		// TODO Auto-generated method stub
-		return this.productDao.findAll();
+	public DataResult<List<Product>> getAll() {
+		    
+			return new SuccessDataResult<List<Product>>
+			(this.productDao.findAll(),"Data listelendi");			
+				
+	}
+
+	@Override
+	public Result add(Product product) {
+		this.productDao.save(product);
+		return new SuccessResult("Ürün eklendi");
 	}
 
 }
